f212ba118b009ff421f4ed9021a8c868917981a1
Jerome C <jcombes@sierrawireless.com>
2016-03-05 17:17:37 +0100

    Update README.md

--- countdown.js
+++ countdown.js
@@ -126,21 +126,6 @@ if (window.jQuery)
             };
         }
 
-        function parse_date(str) {
-            var match = str.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{1,2}:[0-9]{2}:[0-9]{2}$/);
-            if (match !== null && match.length > 0) {
-                var parts = str.split(" ");
-                var date = parts[0].split("-");
-                var time = parts[1].split(":");
-                return new Date(date[0], date[1] - 1, date[2], time[0], time[1], time[2]);
-            }
-            var d = Date.parse(str);
-            if (!isNaN(d)) return d;
-            d = Date.parse(str.replace(/-/g, '/').replace('T', ' '));
-            if (!isNaN(d)) return d;
-            return new Date();
-        }
-
         function parse_times(diff, old_diff, total_duration, units, floor) {
             var raw_time = {};
             var raw_old_time = {};
@@ -656,7 +641,7 @@ if (window.jQuery)
                 attr_data_date = $(this.element).attr('data-date');
             }
             if (typeof attr_data_date === "string") {
-                this.data.attributes.ref_date = Date.parse(attr_data_date); //parse_date(attr_data_date);
+                this.data.attributes.ref_date = Date.parse(attr_data_date);
             } else if (typeof this.data.timer === "number") {
                 if (this.data.paused) {
                     this.data.attributes.ref_date = (new Date()).getTime() + (this.data.timer * 1000);
